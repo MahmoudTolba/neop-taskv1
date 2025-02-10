05ff40ae379307e47aa4452e92630c4ff7655ef0
MahmoudTolba <73529850+MahmoudTolba@users.noreply.github.com>
2025-02-10 13:08:11 +0200

    Update LandingView.vue

--- src/components/LandingView.vue
+++ src/components/LandingView.vue
@@ -1,16 +1,16 @@
 <template>
 <div class="relative overflow-hidden">
     <div class="buble absolute -right-28 -top-5 -z-10">
-      <img src="../assets/" alt="buble" />
+      
     </div>
     <header class="px-8 py-5">
       <nav class="flex items-center justify-between">
         <div class="logo">
-          <img src="../assets/Vector.png" alt="logo" class="cursor-pointer" />
+          <img src="@/assets/Vector.png" alt="logo" class="cursor-pointer" />
         </div>
         <div className="shopping flex items-center gap-2">
-          <img src="../assets/Group 1000010764.png" alt="bag" class="cursor-pointer" />
-          <img src="../assets/fi_5908597.png" alt="bar" class="cursor-pointer" />
+          <img src='@/assets/Group 1000010764.png' alt="bag" class="cursor-pointer" />
+          <img src="@/assets/fi_5908597.png" alt="bar" class="cursor-pointer" />
         </div>
       </nav>
     </header>
@@ -23,7 +23,7 @@
       <div class="hero-text flex w-[300px] flex-col gap-4">
         <div class="relative">
           <img
-            src="../assets/Group.png"
+            src="@/assets/Group.png"
             alt="coffee-piece"
             class="absolute left-4 top-1/2 w-6 translate-y-1"
           />
@@ -40,44 +40,44 @@
           <button class="rounded-full border-2 border-solid px-4 py-2">
             SHOP 20% OFF
           </button>
-          <img src="../assets/Group 1000010764.png" alt="bag" />
+          <img src="@/assets/Group 1000010764.png" alt="bag" />
         </div>
       </div>
       <div class="images flex items-center">
         <div class="w-[500px]">
           <img
-            src="./coffe_bag.png"
+            src="@/assets/Group 1261153045.png"
             alt="coffee-bag"
             class="w-full max-w-full"
           />
         </div>
         <div class="absolute -right-14 flex h-[200px] w-[200px] items-center justify-center rounded-full bg-[#6F4336] p-4">
-          <img src="./cup_2.png" alt="cup" class="w-full max-w-full" />
+          <img src="@/assets/Group 1261153046.png" alt="cup" class="w-full max-w-full" />
         </div>
       </div>
     </main>
     <div class="slide">
       <marquee width="100%">
-        <div class="flex items-center gap-5">
-          <img src="./coffee_piece.png" alt="coffee-piece" />
+        <div class="flex items-center gap-5 w-full justify-between">
+          <img src="@/assets/Group.png" alt="coffee-piece" />
           coffee
-          <img src="./coffee_piece.png" alt="coffee-piece" />
+          <img src="@/assets/Group.png" alt="coffee-piece" />
           coffee
-          <img src="./coffee_piece.png" alt="coffee-piece" />
+          <img src="@/assets/Group.png" alt="coffee-piece" />
           coffee
-          <img src="./coffee_piece.png" alt="coffee-piece" />
+          <img src="@/assets/Group.png" alt="coffee-piece" />
           coffee
-          <img src="./coffee_piece.png" alt="coffee-piece" />
+          <img src="@/assets/Group.png" alt="coffee-piece" />
           coffee
-          <img src="./coffee_piece.png" alt="coffee-piece" />
+          <img src="@/assets/Group.png" alt="coffee-piece" />
           coffee
-          <img src="./coffee_piece.png" alt="coffee-piece" />
+          <img src="@/assets/Group.png" alt="coffee-piece" />
           coffee
-          <img src="./coffee_piece.png" alt="coffee-piece" />
+          <img src="@/assets/Group.png" alt="coffee-piece" />
           coffee
-          <img src="./coffee_piece.png" alt="coffee-piece" />
+          <img src="@/assets/Group.png" alt="coffee-piece" />
           coffee
-          <img src="./coffee_piece.png" alt="coffee-piece" />
+          <img src="@/assets/Group.png" alt="coffee-piece" />
           coffee
         </div>
       </marquee>
